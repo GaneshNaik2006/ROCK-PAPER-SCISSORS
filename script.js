34d9b1bd1c971a4e31414aedbd304f246e5c3244
GaneshNaik2006 <ganeshiitbombay225@gmail.com>
2026-06-13 18:03:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,74 @@
-let userscoree=0;
-let compscoree=0;
+let userscoree = 0;
+let compscoree = 0;
 
-let choices=document.querySelectorAll(".choice");
-let msg=document.querySelector(".msg");
-let userscore=document.querySelector("#userscore");
-let compscore=document.querySelector("#compscore");
-let btn=document.querySelector("button");
-let audio=document.querySelector("audio");
+let choices = document.querySelectorAll(".choice");
+let msg = document.querySelector(".msg");
+let userscore = document.querySelector("#userscore");
+let compscore = document.querySelector("#compscore");
+let btn = document.querySelector("button");
+let audio = document.querySelector("audio");
 
-let resetgame=()=>{
-    userscoree=0;
-    userscore.innerText=userscoree;
-    compscoree=0;
-    compscore.innerText=compscoree;
-    msg.innerText="Play your Game";
+let resetgame = () => {
+    userscoree = 0;
+    userscore.innerText = userscoree;
+    compscoree = 0;
+    compscore.innerText = compscoree;
+    msg.innerText = "Play your Game";
     audio.play();
-    msg.style.backgroundColor="#081b31";
-
-
-}
-let drawgame=()=>{
-    msg.innerText="It's a draw.play again";
-    msg.style.backgroundColor="#081b31";
+    msg.style.backgroundColor = "#081b31";
 }
 
-let showwinner=(userwin,userchoice,compchoice)=>{
-   if(userwin){
-    msg.innerText=`You win! your ${userchoice} beats ${compchoice} `;
-    msg.style.backgroundColor="green";
-    userscoree++;
-    userscore.innerText=userscoree;
-   }
-   else{
-   msg.innerText=`You lost.${compchoice} beats ${userchoice}`;
-   msg.style.backgroundColor="red";
-   compscoree++;
-   compscore.innerText=compscoree;
-   }
-
+let drawgame = () => {
+    msg.innerText = "It's a draw. Play again!";
+    msg.style.backgroundColor = "#081b31";
 }
-let gencomputerchoice=()=>{
-    let options=["rock","paper","scissor"]
-    let randidx=Math.floor(Math.random()*3);
+
+let showwinner = (userwin, userchoice, compchoice) => {
+    if (userwin) {
+        msg.innerText = `You win! Your ${userchoice} beats ${compchoice}`;
+        msg.style.backgroundColor = "green";
+        userscoree++;
+        userscore.innerText = userscoree;
+    } else {
+        msg.innerText = `You lost. ${compchoice} beats your ${userchoice}`;
+        msg.style.backgroundColor = "red";
+        compscoree++;
+        compscore.innerText = compscoree;
+    }
+}
+
+let gencomputerchoice = () => {
+    let options = ["rock", "paper", "scissor"];
+    let randidx = Math.floor(Math.random() * 3);
     return options[randidx];  
 }
-let playgame=(userchoice)=>{
-    console.log("userchoice is",userchoice);
-    //comuterchoice
-    let compchoice=gencomputerchoice();
-    console.log("the computerchoice is:",compchoice);
 
-    if(userchoice===compchoice){
+let playgame = (userchoice) => {
+    console.log("userchoice is", userchoice);
+    let compchoice = gencomputerchoice();
+    console.log("the computerchoice is:", compchoice);
+
+    if (userchoice === compchoice) {
         drawgame();
-    }
-    else{
-        let userwin=true;
-        if(compchoice==="rock"){
-            userwin=userchoice==="paper"? true:false;
+    } else {
+        let userwin = true;
+        if (compchoice === "rock") {
+            userwin = userchoice === "paper" ? true : false;
+        } else if (compchoice === "paper") {
+            userwin = userchoice === "rock" ? false : true;
+        } else {
+            userwin = userchoice === "paper" ? false : true;
         }
-        else if(compchoice==="paper"){
-            userwin=userchoice==="rock"?false:true;
-        }
-        else{
-            userwin=userchoice==="paper"?false:true;
-        }
-        showwinner(userwin,userchoice,compchoice);
+        showwinner(userwin, userchoice, compchoice);
     }
 }
 
-for(let choice of choices){
-    choice.addEventListener("click",()=>{
-        let userchoice=choice.getAttribute("id");
+for (let choice of choices) {
+    choice.addEventListener("click", () => {
+        let userchoice = choice.getAttribute("id");
         playgame(userchoice); 
         audio.play();
-    })
+    });
 }
 
 btn.addEventListener("click", resetgame);
